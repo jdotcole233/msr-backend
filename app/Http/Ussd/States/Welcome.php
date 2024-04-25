d90e3533a3cc1c06c7ed2366dabee31a07062fc3
Jay Cole <jdotcole233@gmail.com>
2024-04-25 21:07:48 +0000

    actor onboarding

--- app/Http/Ussd/States/Welcome.php
+++ app/Http/Ussd/States/Welcome.php
@@ -20,13 +20,17 @@ class Welcome extends State
         $actor = tblActor::where('contactPhone', $this->record->phoneNumber)->first();
         $message = "Welcome to MSR";
         $this->record->set($this->record->sessionId, json_encode([]));
-        $cache_record = json_decode($this->record->get($this->record->sessionId));
 
         if (!empty($actor)) {
             $message = "Welcome " . $actor->name ?? "to MSR";
         }
 
-        if (is_null($actor) && is_object($cache_record)) {
+        info("actor " . json_encode($actor));
+        info("actor " . json_encode(is_null($actor)));
+        // info("actor ". json_encode(is_object($cache_record)));
+
+        if (is_null($actor)) {
+            $cache_record = json_decode($this->record->get($this->record->sessionId));
             $cache_record['init'] = 'START';
             $this->menu->text($message)
                 ->lineBreak(2)
@@ -45,7 +49,7 @@ class Welcome extends State
 
         if (is_object($cache_record) && strcmp($cache_record['init'], "START") === 0) {
             $this->decision->equal("1", RegionState::class)
-            ->any(Error::class);
+                ->any(Error::class);
         } else {
             if (is_object($cache_record) && in_array($argument, [1, 2, 3, 4])) {
                 $cache_record['transactionType'] = $this->transactionTypes[intval($argument) - 1];

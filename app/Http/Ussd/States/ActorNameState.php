cb41a7891ce33934009e66cf0596401ec4022809
Jay Cole <jdotcole233@gmail.com>
2024-04-25 21:20:40 +0000

    actor onboarding

--- app/Http/Ussd/States/ActorNameState.php
+++ app/Http/Ussd/States/ActorNameState.php
@@ -15,7 +15,7 @@ class ActorNameState extends State
     {
         $cache_record = json_decode($this->record->get($this->record->sessionId));
         if (is_object($cache_record) && !empty($argument)) {
-            $cache_record['name'] = $argument;
+            $cache_record->name = $argument;
             $cache_record = json_encode($cache_record);
             $this->record->set($this->record->sessionId, $cache_record);
         }

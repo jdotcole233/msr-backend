0b2cf0f00401886b1815511a8a6e0fa86bcbeb50
Jay Cole <jdotcole233@gmail.com>
2024-05-28 19:34:46 +0000

    session check

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,8 @@ Route::prefix('v1/auth')->group(function () {
 Route::post('v1/warehouse', [AuthController::class, 'store']);
 
 Route::post('v1/ussd', function (Request $request) {
-    info(json_encode($request));
+    info(json_encode($request->all()));
+    info(json_encode($request['MSISDN']));
     $ussd = Ussd::machine()
         ->setFromRequest([
             'user_id' => 'USERID',

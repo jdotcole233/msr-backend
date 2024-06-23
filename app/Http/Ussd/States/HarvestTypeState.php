f0cb467f1e5c8633971eb75736eec52a2471684c
Jay Cole <jdotcole233@gmail.com>
2024-06-23 19:36:06 +0000

    harvest type

--- app/Http/Ussd/States/HarvestTypeState.php
+++ app/Http/Ussd/States/HarvestTypeState.php
@@ -18,11 +18,14 @@ class HarvestTypeState extends State
     protected function afterRendering(string $argument): void
     {
         $cache_record = json_decode($this->record->get($this->record->sessionId));
+
         if (is_object($cache_record)) {
             $cache_record->harvest_type = $this->harvest_type[intval($argument) - 1];
             $cache_record = json_encode($cache_record);
             $this->record->set($this->record->sessionId, $cache_record);
         }
+
+        info("Final ". json_encode($cache_record));
 
         $this->decision->numeric(PlaceOrder::class)
             ->any(Error::class);

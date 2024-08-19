a61f7fd516207b38a056fbbc4f0e86844721b992
Jay Cole <jdotcole233@gmail.com>
2024-08-19 01:18:42 +0000

    documents fees

--- app/Http/Ussd/States/SellOfferPackageSizeState.php
+++ app/Http/Ussd/States/SellOfferPackageSizeState.php
@@ -26,15 +26,16 @@ class SellOfferPackageSizeState extends State
     protected function afterRendering(string $argument): void
     { 
         $cache_record = json_decode($this->record->get($this->record->sessionId));
+        $range = \range(1, count($cache_record->sizes));
 
         if (is_object($cache_record))
         {
-            $cache_record->package_size = $this->package_size[intval($argument) - 1]; 
+            $cache_record->package_size = $cache_record->sizes[intval($argument) - 1]; 
             $cache_record = json_encode($cache_record);
             $this->record->set($this->record->sessionId, $cache_record);
         }
         
-        $range = \range(1, count($this->package_size));
+        // $range = \range(1, count($this->package_size));
         $this->decision->in($range, CommodityQuantitySellOfferState::class)
         ->any(Error::class);
 

4796e8713fc2877c4df91e940fe4bd5883ca96c0
Jay Cole <jdotcole233@gmail.com>
2024-08-19 15:05:15 +0000

    documents fees

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,13 +7,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function getDashboardStats() {
-        $orders = tblOrder::get()->mapToGroups(function ($item, int $key) {
+        $warehouse = Auth::user()->operator->fkWarehouseIDNo;
+        info("warehouse id for stats ". json_encode($warehouse));
+        $orders = tblOrder::where('fkWarehouseIDNo', $warehouse)->get()->mapToGroups(function ($item, int $key) {
             return [$item['transactionType'] => $item['id']];
         })->map(function ($item) {
             return collect($item)->count();
